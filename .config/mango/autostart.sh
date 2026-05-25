5462f45db3cd2668f5826af5ff6da5339d85fda6
James Robert Rooke <thepoltergeist23@gmail.com>
2026-05-25 21:43:41 +0800

    Update config mango

--- .config/mango/autostart.sh
+++ .config/mango/autostart.sh
@@ -3,13 +3,6 @@
 
 set +e
 
-# Import environment for systemd
-systemctl --user import-environment DISPLAY WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP XDG_SESSION_TYPE
-
-/usr/libexec/xdg-desktop-portal-wlr &
-
-# gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
-# gsettings set org.gnome.desktop.interface cursor-size 24 
-# gsettings set org.gnome.desktop.interface cursor-theme "Twilight-cursors"
-# Noctalia shell (your preferred launcher/panel)
+systemctl --user start mangowc-session.target
+# Noctalia shell
 qs -c noctalia-shell &
