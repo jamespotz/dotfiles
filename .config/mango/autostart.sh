a288e4ea99b7468eff836b1b5254bb83a13fbcf3
James Robert Rooke <thepoltergeist23@gmail.com>
2026-05-27 00:09:20 +0800

    update mango and niri configs

---
+++ .config/mango/autostart.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set +e
+
+# some env can't auto run the portal, so need this
+/usr/libexec/xdg-desktop-portal-wlr >/dev/null 2>&1 &
+
+wl-paste --watch cliphist store &
+
+systemctl --user start mango-session.target
+
+dms run >/dev/null 2>&1 &
