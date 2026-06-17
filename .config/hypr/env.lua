310fe1601d4cfab93e4608ee4b10f7ba02e9b0ff
James Robert Rooke <thepoltergeist23@gmail.com>
2026-06-18 02:43:27 +0800

    update hyprland config autostart and env

--- .config/hypr/env.lua
+++ .config/hypr/env.lua
@@ -1,3 +1,10 @@
+local home_dir = os.getenv("HOME")
+
+local xdg_data_dirs_old = os.getenv("XDG_DATA_DIRS") or ""
+hl.env("XDG_DATA_DIRS",
+  home_dir ..
+  "/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share:" .. xdg_data_dirs_old)
+
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
@@ -7,3 +14,4 @@ hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
 hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 hl.env("NVD_BACKEND", "direct")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
