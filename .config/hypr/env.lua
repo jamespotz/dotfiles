0f111aa894252ecf0e579f579e9b2926d0562e6d
James Robert Rooke <thepoltergeist23@gmail.com>
2026-09-01 21:17:21 +0800

    update config

--- .config/hypr/env.lua
+++ .config/hypr/env.lua
@@ -2,11 +2,12 @@ local home_dir = os.getenv("HOME")
 
 local xdg_data_dirs_old = os.getenv("XDG_DATA_DIRS") or ""
 hl.env("XDG_DATA_DIRS",
-    home_dir ..
-    "/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share:" ..
-    xdg_data_dirs_old)
+  home_dir ..
+  "/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share:" ..
+  xdg_data_dirs_old)
 
 hl.env("XCURSOR_SIZE", "24")
+hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
 hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
 hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
@@ -17,3 +18,4 @@ hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
 hl.env("NVD_BACKEND", "direct")
 hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
 hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("HYPRGLASS_SKIP_VERSION_CHECK", "1")
--- .config/hypr/plugins/overview.lua
+++ .config/hypr/plugins/overview.lua
@@ -1,23 +1,26 @@
 -- .config/hypr/hyprland.lua
-hl.config({
-  plugin = {
-    scrolloverview = {
-      gesture_distance = 300,       -- how far is the "max" for the gesture
-      scale = 0.5,                  -- preferred overview scale
-      workspace_gap = 100,
-      layout = "vertical",          -- vertical or horizontal
-      wallpaper = 2,                -- 0: global only, 1: per-workspace only, 2: both
-      blur = true,                  -- blur only the main overview wallpaper
 
-      shadow = {
-        enabled = true,
-        range = 50,
+if hl.plugin.scrolloverview then
+  hl.config({
+    plugin = {
+      scrolloverview = {
+        gesture_distance = 300, -- how far is the "max" for the gesture
+        scale = 0.5,            -- preferred overview scale
+        workspace_gap = 100,
+        layout = "vertical",    -- vertical or horizontal
+        wallpaper = 2,          -- 0: global only, 1: per-workspace only, 2: both
+        blur = true,            -- blur only the main overview wallpaper
+
+        shadow = {
+          enabled = true,
+          range = 50,
+        },
       },
     },
-  },
-})
+  })
 
--- Toggle ScrollOverview with SUPER+g
-hl.bind("SUPER + g", function()
-  hl.plugin.scrolloverview.overview("toggle all")
-end)
+  -- Toggle ScrollOverview with SUPER+g
+  hl.bind("SUPER + g", function()
+    hl.plugin.scrolloverview.overview("toggle all")
+  end)
+end
