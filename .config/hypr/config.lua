4fd59aea6024ea4a22685d4e18ba94745bec5187
James Robert Rooke <thepoltergeist23@gmail.com>
2026-09-03 02:23:20 +0800

    update config and window rules

--- .config/hypr/config.lua
+++ .config/hypr/config.lua
@@ -2,100 +2,111 @@
 -- INPUT CONFIG
 -- ==================
 hl.config({
-    input = {
-        kb_layout = "us",
-        numlock_by_default = true,
-        sensitivity = 0,
-        accel_profile = "flat",
-        force_no_accel = true,
-        follow_mouse = 2
-    },
+  input = {
+    kb_layout = "us",
+    numlock_by_default = true,
+    sensitivity = 0,
+    accel_profile = "flat",
+    force_no_accel = true,
+    follow_mouse = 2
+  },
 })
 
 -- ==================
 -- GENERAL LAYOUT
 -- ==================
 hl.config({
-    general = {
-        gaps_in = 5,
-        gaps_out = 5,
-        border_size = 2,
-        layout = "scrolling",
+  general = {
+    gaps_in = 5,
+    gaps_out = 5,
+    border_size = 2,
+    layout = "scrolling",
+  },
+
+})
+
+hl.config({
+  decoration = {
+    rounding = 20,
+    rounding_power = 2,
+    active_opacity = 0.89,
+    inactive_opacity = 0.89,
+
+    shadow = {
+      enabled = true,
+      range = 28,
+      render_power = 3,
+      color = 0x890a0a0a,
     },
 
-    decoration = {
-        rounding = 20,
-        rounding_power = 2,
-
-        shadow = {
-            enabled = true,
-            range = 4,
-            render_power = 3,
-            color = 0xee1a1a1a,
-        },
-
-        blur = {
-            enabled = true,
-            size = 3,
-            passes = 2,
-            vibrancy = 0.1696,
-        },
+    blur = {
+      enabled = true,
+      brightness = 0.8,
+      contrast = 2,
+      passes = 2,
+      vibrancy = 0.35,
+      vibrancy_darkness = 0.35,
+      noise = 0,
+      size = 10,
+      special = false
     },
+  },
+
 })
 
 -- See https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/ for more
 hl.config({
-    dwindle = {
-        preserve_split = true, -- You probably want this
-    },
+  dwindle = {
+    preserve_split = true, -- You probably want this
+  },
 })
 
 -- See https://wiki.hypr.land/Configuring/Layouts/Master-Layout/ for more
 hl.config({
-    master = {
-        new_status = "master",
-    },
+  master = {
+    new_status = "master",
+  },
 })
 
 -- See https://wiki.hypr.land/Configuring/Layouts/Scrolling-Layout/ for more
 hl.config({
-    scrolling = {
-        fullscreen_on_one_column = true,
-        follow_min_visible = 1,
-    },
+  scrolling = {
+    fullscreen_on_one_column = true,
+    follow_min_visible = 1,
+  },
 })
 
 -- ==================
 -- MISC
 -- ==================
 hl.config({
-    misc = {
-        disable_hyprland_logo      = true,
-        force_default_wallpaper    = 0,
-        disable_splash_rendering   = true,
-        initial_workspace_tracking = 1,
-        on_focus_under_fullscreen  = 1,
-        allow_session_lock_restore = true,
-        focus_on_activate          = true
-    },
+  misc = {
+    disable_hyprland_logo      = true,
+    force_default_wallpaper    = 0,
+    disable_splash_rendering   = true,
+    initial_workspace_tracking = 1,
+    on_focus_under_fullscreen  = 1,
+    allow_session_lock_restore = true,
+    focus_on_activate          = true
+  },
 })
 
 -- ==================
 -- BINDS
 -- ==================
 hl.config({
-    binds = {
-        workspace_back_and_forth = true,
-        workspace_center_on = 1,
-    },
+  binds = {
+    workspace_back_and_forth = true,
+    workspace_center_on = 1,
+  },
 })
 
 -- unscale XWayland
 hl.config({
-    xwayland = {
-        force_zero_scaling = true
-    },
-    cursor = {
-        no_hardware_cursors = true
-    },
+  xwayland = {
+    force_zero_scaling = true
+  },
+  cursor = {
+    no_hardware_cursors = true
+  },
 })
