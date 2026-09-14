9e0a8d7cc3175fe5d08ed1a06b668dcc782ae04b
James Robert Rooke <thepoltergeist23@gmail.com>
2026-09-14 22:01:57 +0800

    Update configs

--- .config/hypr/modules/general.lua
+++ .config/hypr/modules/general.lua
@@ -38,11 +38,11 @@ hl.config({
     focus_fit_method         = 1,
     column_width             = 0.5,
     follow_focus             = true,
-    follow_min_visible       = 0.0,
-    explicit_column_widths   = "0.35, 0.5, 0.65, 1.0",
+    follow_min_visible       = 1,
+    explicit_column_widths   = "0.35, 0.5, 0.65, 1.0"
   },
 
   xwayland = {
     force_zero_scaling = true
-  },
+  }
 })
